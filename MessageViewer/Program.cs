d6eacc67d181903b3ee985ab9bf6e185ac9bbe38
MohammadAyaad <mohammadayaad256@gmail.com>
2024-05-06 14:27:04 +0300

    Add project files.

---
+++ MessageViewer/Program.cs
@@ -0,0 +1,45 @@
+﻿
+string spacer = "            ";
+void NL()
+{
+    Console.Write("\n" + spacer);
+}
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+string Message = "Hello........\nI'm Very sorry about all i did before.........\nAnd........ I Promise you .....that i'll try my best to change it.....\nLove you.! .....<3";
+
+Console.Write(spacer);
+
+for (int i = 0;i < Message.Length;i++)
+{
+    Thread.Sleep(75);
+    switch(Message[i])
+    {
+        case '\\':
+            i++;
+            Console.Write(Message[i]);
+            break;
+        case '.':
+            Thread.Sleep(100);
+            break;
+        case '\n':
+            Thread.Sleep(250);
+            NL();
+            break;
+        default:
+            Console.Write(Message[i]);
+            break;
+    }
+}
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+
